f240f8183f334c4a3764b4091ebaafd707ab115f
kostya solyanik <kosjudge@gmail.com>
2016-09-22 00:22:22 +0300

    profile

--- www/js/controllers/ProfileCtrl.js
+++ www/js/controllers/ProfileCtrl.js
@@ -1,3 +1,25 @@
-restaurant.controller('ProfileCtrl', function($scope) {})
+restaurant.controller('ProfileCtrl', function ($scope, $ionicPopup) {
+  $scope.editUser = function () {
+    var myPopup = $ionicPopup.show({
+      template: '<input type="text" style="border: 1px solid #cfcfcf;">',
+      title: 'Введите новые данные',
+      scope: $scope,
+      buttons: [
+        {text: 'Отменить'}, {
+          text: '<b>Сохранить</b>',
+          type: 'button-positive',
+          onTap: function () {
+          }
+        }
+      ]
+    });
+    myPopup.then(function (res) {
+      console.log('Tapped!', res);
+    });
+    //$timeout(function () {
+    //  myPopup.close();
+    //}, 3000);
+  };
+});
 
 
